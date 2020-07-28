23ff2f9399463dc241f8f0683f9c5e77a2894cc8
Bryon Sigawin <zh3nlong@gmail.com>
2020-07-28 22:56:42 +0800

    test preview

--- src/cms/preview-templates/IndexPagePreview.js
+++ src/cms/preview-templates/IndexPagePreview.js
@@ -1,32 +1,25 @@
 import React from 'react'
-import PropTypes from 'prop-types'
-import { IndexPageTemplate } from '../../templates/index-page'
+import IndexPage from '../../templates/index-page'
+import { ThemeProvider } from 'styled-components'
 
 const IndexPagePreview = ({ entry, getAsset }) => {
   const data = entry.getIn(['data']).toJS()
 
+  console.log(data)
+
   if (data) {
     return (
-      <IndexPageTemplate
-        image={getAsset(data.image)}
-        title={data.title}
-        heading={data.heading}
-        subheading={data.subheading}
-        description={data.description}
-        intro={data.intro || { blurbs: [] }}
-        mainpitch={data.mainpitch || {}}
-      />
+      <ThemeProvider theme={{ primaryColor: 'red' }}>
+        <IndexPage
+          data={{
+            markdownRemark: { html: data.body || '' },
+          }}
+        />
+      </ThemeProvider>
     )
   } else {
     return <div>Loading...</div>
   }
-}
-
-IndexPagePreview.propTypes = {
-  entry: PropTypes.shape({
-    getIn: PropTypes.func,
-  }),
-  getAsset: PropTypes.func,
 }
 
 export default IndexPagePreview
--- src/templates/index-page/index.js
+++ src/templates/index-page/index.js
@@ -1,82 +1,3 @@
-// import React, { useState, useRef, useEffect } from 'react'
-// import { graphql } from 'gatsby'
-// import { motion, AnimatePresence, AnimateSharedLayout } from 'framer-motion'
-
-// import Layout from '../../components/Layout'
-
-// import styles from './styles.module.scss'
-
-// const ShowcaseItem = ({ item, isVisible }) => (
-//   <AnimatePresence>
-//     {isVisible && (
-//       <motion.div
-//         layout
-//         className={styles.showcaseItem}
-//         initial={false}
-//         animate={{ opacity: 1 }}
-//         exit={{ opacity: 0 }}
-//       >
-//         <div className={styles.showcaseItemInner}>
-//           <p>Item: {item}</p>
-//         </div>
-//       </motion.div>
-//     )}
-//   </AnimatePresence>
-// )
-
-// const IndexPage = ({ data }) => {
-//   const { html: content } = data.markdownRemark
-
-//   const items = ['DESIGN', 'MARKETING', 'DESIGN']
-
-//   const [currentFilter, setCurrentFilter] = useState('SHOW ALL')
-
-//   const checkVisible = (item) => (currentFilter === 'SHOW ALL' ? true : item === currentFilter)
-
-//   return (
-//     <Layout>
-//       <div className="hero is-danger">
-//         <div className="hero-body has-text-centered" dangerouslySetInnerHTML={{ __html: content }} />
-//       </div>
-
-//       <div className="section px-0">
-//         <div className={styles.filter}>
-//           <div onClick={() => setCurrentFilter('SHOW ALL')} className={styles.filterItemSelected}>
-//             SHOW ALL
-//           </div>
-//           <div onClick={() => setCurrentFilter('DESIGN')} className={styles.filterItem}>
-//             DESIGN
-//           </div>
-//           <div onClick={() => setCurrentFilter('MARKETING')} className={styles.filterItem}>
-//             MARKETING
-//           </div>
-//           <div onClick={() => setCurrentFilter('STRATEGIC')} className={styles.filterItem}>
-//             STRATEGIC
-//           </div>
-//         </div>
-
-//         <AnimateSharedLayout>
-//           <motion.div layout className={styles.showcase}>
-//             {items.map((item, index) => (
-//               <ShowcaseItem item={item} key={index} isVisible={checkVisible(item)} />
-//             ))}
-//           </motion.div>
-//         </AnimateSharedLayout>
-//       </div>
-//     </Layout>
-//   )
-// }
-
-// export default IndexPage
-
-// export const pageQuery = graphql`
-//   query IndexPageTemplate {
-//     markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
-//       html
-//     }
-//   }
-// `
-
 import React, { useContext, useState } from 'react'
 import Container from '@components/Container'
 import Layout from '@components/Layout'
@@ -84,7 +5,6 @@ import SEO from '@components/SEO'
 import Section from '@components/Section'
 
 import { ThemeContext } from 'styled-components'
-import { Headline, SubHeadline } from '@components/Typography'
 import PortfolioItem from './PortfolioItem'
 import Grid from '@components/Grid'
 import { Filter, FilterItem } from './styles'
