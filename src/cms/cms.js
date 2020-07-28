c8f20975e9cadb7c9d42dbc08dd650f8f14d43d2
Bryon Sigawin <zh3nlong@gmail.com>
2020-07-28 23:04:06 +0800

    add styled-components injector

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,6 +2,8 @@ import CMS from 'netlify-cms-app'
 import uploadcare from 'netlify-cms-media-library-uploadcare'
 import cloudinary from 'netlify-cms-media-library-cloudinary'
 
+import withStyledComponentsRendered from '@hooks/withStyledComponentsRendered'
+
 // import AboutPagePreview from './preview-templates/AboutPagePreview'
 // import BlogPostPreview from './preview-templates/BlogPostPreview'
 // import ProductPagePreview from './preview-templates/ProductPagePreview'
@@ -10,7 +12,7 @@ import IndexPagePreview from './preview-templates/IndexPagePreview'
 CMS.registerMediaLibrary(uploadcare)
 CMS.registerMediaLibrary(cloudinary)
 
-CMS.registerPreviewTemplate('index', IndexPagePreview)
+CMS.registerPreviewTemplate('index', withStyledComponentsRendered(IndexPagePreview))
 // CMS.registerPreviewTemplate('about', AboutPagePreview)
 // CMS.registerPreviewTemplate('products', ProductPagePreview)
 // CMS.registerPreviewTemplate('blog', BlogPostPreview)
