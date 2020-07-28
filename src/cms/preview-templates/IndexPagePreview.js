5e09dd1af625239e24bcc37cd708e4c851c09469
Bryon Sigawin <zh3nlong@gmail.com>
2020-07-28 23:11:40 +0800

    update props

--- src/cms/preview-templates/IndexPagePreview.js
+++ src/cms/preview-templates/IndexPagePreview.js
@@ -2,24 +2,16 @@ import React from 'react'
 import IndexPage from '../../templates/index-page'
 import { ThemeProvider } from 'styled-components'
 
-const IndexPagePreview = ({ entry, getAsset }) => {
-  const data = entry.getIn(['data']).toJS()
-
-  console.log(data)
-
-  if (data) {
-    return (
-      <ThemeProvider theme={{ primaryColor: 'red' }}>
-        <IndexPage
-          data={{
-            markdownRemark: { html: data.body || '' },
-          }}
-        />
-      </ThemeProvider>
-    )
-  } else {
-    return <div>Loading...</div>
-  }
+const IndexPagePreview = ({ widgetFor }) => {
+  return (
+    <ThemeProvider theme={{ primaryColor: 'red' }}>
+      <IndexPage
+        data={{
+          markdownRemark: { html: widgetFor('body') },
+        }}
+      />
+    </ThemeProvider>
+  )
 }
 
 export default IndexPagePreview
