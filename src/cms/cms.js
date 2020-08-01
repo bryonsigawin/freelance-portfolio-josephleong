402459300abfbdbb1603fe49c0342249502c6b88
Bryon Sigawin <zh3nlong@gmail.com>
2020-08-01 18:27:59 +0800

    update deps

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,17 +1,9 @@
 import CMS from 'netlify-cms-app'
-import uploadcare from 'netlify-cms-media-library-uploadcare'
-import cloudinary from 'netlify-cms-media-library-cloudinary'
 
 import withStyledComponentsRendered from '@hooks/withStyledComponentsRendered'
 
 import IndexPagePreview from './preview-templates/IndexPagePreview'
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 
-CMS.registerMediaLibrary(uploadcare)
-CMS.registerMediaLibrary(cloudinary)
-
 CMS.registerPreviewTemplate('index', withStyledComponentsRendered(IndexPagePreview))
 CMS.registerPreviewTemplate('index', withStyledComponentsRendered(AboutPagePreview))
-// CMS.registerPreviewTemplate('about', AboutPagePreview)
-// CMS.registerPreviewTemplate('products', ProductPagePreview)
-// CMS.registerPreviewTemplate('blog', BlogPostPreview)
