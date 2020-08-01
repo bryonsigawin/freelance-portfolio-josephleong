099178758aa2ea94244abd079a1f3780082d6775
Bryon Sigawin <zh3nlong@gmail.com>
2020-08-01 22:43:26 +0800

    update

--- src/cms/preview-templates/TheRealDealPagePreview.js
+++ src/cms/preview-templates/TheRealDealPagePreview.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import { TheRealDealTemplate } from '../../templates/the-real-deal-page'
+import { TheRealDealPageTemplate } from '../../templates/the-real-deal-page'
 import { ThemeProvider } from 'styled-components'
 
 const TheRealDealPreview = ({ widgetFor }) => {
   return (
     <ThemeProvider theme={{ primaryColor: 'red' }}>
-      <TheRealDealTemplate content={widgetFor('body')} />
+      <TheRealDealPageTemplate content={widgetFor('body')} />
     </ThemeProvider>
   )
 }
