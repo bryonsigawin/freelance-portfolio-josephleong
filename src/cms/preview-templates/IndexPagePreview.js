4c170a3f0e69a1d6ecba7593ededdc834bdf0846
Bryon Sigawin <zh3nlong@gmail.com>
2020-08-23 02:14:22 +0800

    preview update

--- src/cms/preview-templates/IndexPagePreview.js
+++ src/cms/preview-templates/IndexPagePreview.js
@@ -2,16 +2,10 @@ import React from 'react'
 import { IndexPageTemplate } from '../../templates/index-page'
 import { ThemeProvider } from 'styled-components'
 
-const IndexPagePreview = ({ entry, widgetFor, widgetsFor }) => {
+const IndexPagePreview = ({ entry, widgetFor }) => {
   const portfolioHighlights = entry.getIn(['data', 'portfolioHighlight'])
 
-  console.log('widgetsFor: portfolio', widgetsFor('portfolioHighlight'))
-  console.log('----------------------------------------------')
-  console.log('plain', portfolioHighlights)
   console.log('toJS', portfolioHighlights.toJS())
-  console.log('----------------------------------------------')
-  console.log('getEntry: body', entry.getIn(['data', 'body']))
-  console.log('widgetFor: body', widgetFor('body'))
 
   return (
     <ThemeProvider theme={{ primaryColor: 'red' }}>
