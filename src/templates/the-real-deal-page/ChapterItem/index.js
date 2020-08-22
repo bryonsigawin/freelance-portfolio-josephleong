e9b216f0d88a3825045818fea3b8998c746cc5d8
Bryon Sigawin <zh3nlong@gmail.com>
2020-08-23 04:37:09 +0800

    fix

--- src/templates/the-real-deal-page/ChapterItem/index.js
+++ src/templates/the-real-deal-page/ChapterItem/index.js
@@ -7,18 +7,18 @@ import { StyledChapterItem, Title, Subtitle, Content, ChapterLink, Image } from 
 
 const mockData = {
   title: 'Lorem ipsum dolor sit.',
-  subtitlte: 'Lorem ipsum dolor sit.',
+  subtitle: 'Lorem ipsum dolor sit.',
   description:
     'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione fuga itaque magni laudantium ea, vero voluptas! Quae et minima neque ipsa tempore dignissimos corporis, optio ipsam, harum, quaerat accusamus iusto.',
 }
 
-const ChapterItem = ({ data = mockData, permalink }) => (
+const ChapterItem = ({ data, permalink }) => (
   <StyledChapterItem>
     <Grid columns="1fr 0.6fr" gap="2rem" alignItems="center">
       <ContentContainer fontSize="1.1rem">
-        <Subtitle>{data.subtitle}</Subtitle>
-        <Title>{data.title}</Title>
-        <Content dangerouslySetInnerHTML={{ __html: data.description }} />
+        <Subtitle>{data.subtitle || mockData.subtitle}</Subtitle>
+        <Title>{data.title || mockData.subtitle}</Title>
+        <Content dangerouslySetInnerHTML={{ __html: data.description || mockData.description }} />
         <ChapterLink to={permalink}>Read Journal</ChapterLink>
       </ContentContainer>
 
